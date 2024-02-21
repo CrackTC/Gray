4b7cfd9e01f9fd19e1e27c4922e9f4b856baf058
CrackTC <chenrke369@gmail.com>
2024-02-21 09:54:30 +0800

    init commit

---
+++ src/Gray.cs
@@ -0,0 +1,16 @@
+﻿using SkiaSharp;
+
+namespace Gray;
+
+public static class SKImageExtensions
+{
+    public static SKImage ToGray(this SKImage image)
+    {
+        var info = image.Info;
+        var surface = SKSurface.Create(new SKImageInfo(info.Width, info.Height, SKColorType.Gray8));
+        var canvas = surface.Canvas;
+        canvas.Clear(SKColors.White);
+        canvas.DrawImage(image, 0, 0);
+        return surface.Snapshot();
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,16 @@
+﻿using SkiaSharp;
+
+namespace Gray.Test;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("path: ");
+        string path = Console.ReadLine() ?? throw new ArgumentNullException(nameof(path));
+        var fileName = Path.GetFileName(path);
+        var image = SKImage.FromEncodedData(path);
+        var gray = image.ToGray();
+        gray.Encode(SKEncodedImageFormat.Jpeg, 80).SaveTo(File.OpenWrite(fileName + "_gray.png"));
+    }
+}
